0a682461023da360925ccc5ded7384d9f0c7cd7d
NicMarvin <marvinmatamoros02@gmail.com>
2018-02-22 19:08:16 -0600

    ya web terminada

--- sendemail.php
+++ sendemail.php
@@ -1,15 +1,27 @@
 <?php
-$name       = @trim(stripslashes($_POST['name']));
-$from       = @trim(stripslashes($_POST['email'])); 
-$subject    = @trim(stripslashes($_POST['subject'])); 
-$message    = @trim(stripslashes($_POST['message']));
-$to          = 'augustohotel2018@gmail.com';
+//llamando a los campos
 
-$headers = "MIME-Version: 1.0";
-$headers .= "Content-type: text/plain; charset=iso-8859-1";
-$headers .= "From: {$name} <{$from}>";
-$headers .= "Reply-To: <{$from}>";
-$headers .= "Subject: {$subject}";
-$headers .= "X-Mailer: PHP/".phpversion();
-mail($to, $subject, $message, $headers);die;
+$nombre       = @trim(stripslashes($_POST['nombre']));
+$correo       = @trim(stripslashes($_POST['correo'])); 
+$telefono    = @trim(stripslashes($_POST['telefono'])); 
+$mensaje    = @trim(stripslashes($_POST['mensaje']));
+
+//datos para el correo
+$destinatario = 'augustohotel2018@gmail.com';
+$asunto = 'Contacto desde nuestra web';
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "telefono: $telefono \n";
+$carta .= "Mensaje: $mensaje ";
+
+//enviando mensaje
+$send = mail($destinatario, $asunto, $carta);
+
+if($send) 
+{ 
+echo '<script language="javascript"> alert("El mensaje ha sido enviado correctamente. Gracias por preferirnos"); </script>'; 
+echo '<script language="JavaScript"> window.location.href ="index.html" </script>';
+} 
+
 ?>
